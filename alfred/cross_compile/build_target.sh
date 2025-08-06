2676d4c79baa3e2b462b23aea1f32afbd1c79b27
arnix <arnaud@olearo-hote.eu>
2025-08-06 12:33:22 +0200

    fix: fix target build script

--- alfred/cross_compile/build_target.sh
+++ alfred/cross_compile/build_target.sh
@@ -6,6 +6,8 @@ sudo apt-get update
 
 cd /ros2_ws
 
+rm -fr install/target
+
 rosdep install -ry \
     --rosdistro humble \
     --from-paths src \
@@ -14,6 +16,7 @@ rosdep install -ry \
 colcon --log-base log/target \
        build --build-base build/target \
              --install-base install/target \
-             --symlink-install \
              --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
              --no-warn-unused-cli
+
+chown -R 1000:1000 build/target install/target log/target
