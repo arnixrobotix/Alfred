c23a5422f80f86c1c0b91b6298ccba71b18356d1
Arnix <arnaud@olearo-hote.eu>
2026-01-03 19:10:17 +0100

    feat: add inline template function for equality with tolerance

--- alfred/src/utils/common_utils/include/common.hpp
+++ alfred/src/utils/common_utils/include/common.hpp
@@ -86,6 +86,11 @@ enum class EdgeChangeType
   undefined
 };
 
+template<class T>
+inline bool AreEqual(T a, T b, T epsilon)
+{
+  return std::fabs(a - b) < epsilon;
+}
 
 using LifecycleCallbackReturn_t =
   rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
